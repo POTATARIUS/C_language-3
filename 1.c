d63b094bf912cd07d58b03bfa2d05841028cbf7e
POTATO <dragaden9@gmail.com>
2023-07-14 17:24:25 +1000

    New feature
    Fix and feature

--- 1.c
+++ 1.c
@@ -4,16 +4,138 @@
 #include <stdbool.h>
 
 int py, px; // @ coords
-int ty, tx; // t coord
 bool t_placed = 0; // flag for goblin
 bool p_placed = 0; // flag for player
 int r_placed = 0; // flag for room
 int p_gold = 0; // player gold
+int dlvl = 1;
 
-int game_loop(int c, int rows, int cols, char (* map)[cols]) // char (* map)[cols]) - two-dimensional array ...
-{	
-	srand(time(NULL));
+struct monsters
+{
+	int y;
+	int x;
+	int hp;
+};
+
+struct monsters monster[10]; // 0 ... 9
+
+
+int battle(int cols, char (* map)[cols], int dir_y, int dir_x)
+{
+	for (int m = 0; m < 10; m++)
+	{
+			if (dir_y == monster[m] .y && dir_x == monster[m] .x)
+			{
+				if (monster[m] .hp <= 0)
+				{
+					map[dir_y][dir_x] = ' ';
+					p_gold += rand() % 10 + 1;
+				}
+				else
+					monster[m] .hp -= 1;
+				break;
+			}
+	}
+	return 0;
+}
+
+int p_action(int c, int cols, char (* map)[cols])
+{
+	int dir_y = py, dir_x = px;
+		
+	if (c == KEY_UP)
+		dir_y--;
+	else if (c == KEY_DOWN)
+		dir_y++;
+	else if (c == KEY_LEFT)
+		dir_x--;
+	else if (c == KEY_RIGHT)
+		dir_x++;
+	else if (c == '>' && map[py][px] == '>')
+	{
+		t_placed = 0;
+		p_placed = 0;
+		r_placed = 0;
+		return 1;
+	}
 	
+	if (map[dir_y][dir_x] == ' ' || map[dir_y][dir_x] == '>')
+	{
+		py = dir_y;
+		px = dir_x;
+	}
+	else if (map[dir_y][dir_x] == 't')
+		battle(cols, map, dir_y, dir_x);
+	return 0;
+}
+
+int dungeon_draw (int rows, int cols, char (* map)[cols])
+{
+	// draw the dungeon
+	for (int y = 0; y <= rows; y++)
+    {
+        for (int x = 0; x <= cols; x++)
+        {
+			if (y == 0 || y == rows)
+				mvaddch(y,x,' ');
+			else if (map[y][x] == 't')
+				mvaddch(y,x,'t');
+			else if (map[y][x] == '>')
+				mvaddch(y,x,'>');
+			else if (map[y][x] == '%')
+				mvaddch(y,x,'%');
+			else if (map[y][x] == ' ')
+				mvaddch(y,x,' ');
+			else
+				mvaddch(y,x,'#');
+        }
+    }
+	
+	mvprintw(rows, 0, "Gold: %d \t Dlvl: %d", p_gold, dlvl);
+	
+	return 0;
+}
+
+int respawn_creatures(int rows, int cols, char (* map)[cols])
+{
+	if (!t_placed) // aka t_placed == 0
+	{
+		int my, mx;
+		
+		for (int m = 0; m < 10; m++)
+		{
+			do
+			{
+				my = rand() % rows;
+				mx = rand() % cols;
+			}
+			while (map[my][mx] == '#' || map [my][mx] == '%') ;
+			
+			monster[m] .y = my;
+			monster[m] .x = mx;
+			monster[m] .hp = 2;
+			map[monster[m] .y][monster[m] .x] = 't';
+		}	
+		
+		t_placed = 1;
+		
+	}
+	
+	if (!p_placed) // aka t_placed == 0
+	{
+		do
+		{
+			py = rand() % rows;
+			px = rand() % cols;
+		}
+		while (map[py][px] == '#' || map [py][px] == '%') ;
+		p_placed = 1;
+	}
+	return 0;
+}
+
+int dungeon_gen (int rows, int cols, char (* map)[cols])
+{
 	if (!r_placed) // r_placed = 0
 	{
 		int ry, rx; // room coords
@@ -30,7 +152,8 @@ int game_loop(int c, int rows, int cols, char (* map)[cols]) // char (* map)[col
 			for (int x = 0; x <= cols; x++)
 			{
 				//borders
-				if (y == 0 || y == rows - 1 || x == 0 || x == cols || y == rows)
+				if (y == 0 || y == 1|| y == rows - 1 || x == 0 ||
+					x == cols || y == rows)
 					map[y][x] = '%';
 				//walls
 				else
@@ -80,7 +203,7 @@ int game_loop(int c, int rows, int cols, char (* map)[cols]) // char (* map)[col
 				}	
 			}
 			while (collision == 1);
-			
+			{
 				// fill DB map with rooms
 				for (int y = ry; y <= ry + r_size_y; y++)
 				{
@@ -131,88 +254,76 @@ int game_loop(int c, int rows, int cols, char (* map)[cols]) // char (* map)[col
 							x--;
 					}
 				}
+			}
 		}
-    }
-	
-	// draw the dungeon
-	for (int y = 0; y <= rows; y++)
-    {
-        for (int x = 0; x <= cols; x++)
-        {
-			if (y == rows)
-				mvaddch(y,x,' ');
-			else if (map[y][x] == '%')
-				mvaddch(y,x,'%');
-			else if (map[y][x] == ' ')
-				mvaddch(y,x,' ');
-			else
-				mvaddch(y,x,'#');
-        }
-    }
-
-    if (c == KEY_UP && map[py-1][px] == ' ') py--;
-    else if (c == KEY_DOWN && map[py+1][px] == ' ') py++;
-    else if (c == KEY_LEFT && map[py][px-1] == ' ') px--;
-    else if (c == KEY_RIGHT && map[py][px+1] == ' ') px++;
-	
-	if (!p_placed) // aka t_placed == 0
-	{
+		
+		// staircase gen
+		int sy, sx;
 		do
 		{
-			py = rand() % rows;
-			px = rand() % cols;
+			sy = rand() % rows;
+			sx = rand() % cols;
 		}
-		while (map[py][px] == '#' || map [py][px] == '%') ;
-		p_placed = 1;
-	}
-	
-	if (!t_placed) // aka t_placed == 0
-	{
-		do
-		{
-			ty = rand() % rows;
-			tx = rand() % cols;
-		}
-		while (map[ty][tx] == '#' || map [ty][tx] == '%') ;
-		t_placed = 1;
-	}
-	
-	if (py == ty && px == tx)
-	{
-		t_placed = 0;
-		p_gold += rand() % 10 + 1;
-	}
-	
-    mvaddch(py,px,'@'); // print gamer
-	mvaddch(ty,tx,'t'); // print goblin
-	mvprintw(rows, 0, "Gold: %d", p_gold);
+		while (map[sy][sx] != ' ');
+		map[sy][sx] = '>';
+		
+    }
 	
 	return 0;
+}
+
+int game_loop(int rows, int cols, char (* map)[cols]) // char (* map)[cols]) - two-dimensional array ...
+{	
+	int c;
+	int new_lvl;
+	srand(time(NULL));
+	
+	dungeon_gen(rows, cols, map);
+
+	respawn_creatures (rows, cols, map);	
+	
+	if (c != 0)
+	{	
+		new_lvl = p_action (c, cols, map); // battle ()
+	}
+	
+	dungeon_draw (rows, cols, map);
+	
+    mvaddch(py,px,'@'); // draw p
+	
+	if (new_lvl)
+		mvprintw(0, 0, "Welcome to level %d (Peress any key to continue)", ++dlvl);	
+	
+	c = getch();
+	
+	return c;
 }
 
 
 int main(void)
 {
     int c = 0; // input
-    int cols, rows; 
+    int rows, cols;	
 
-    initscr();
-    keypad(stdscr, 1); // allow arrows
-    noecho(); // don't display input
+    initscr(); // init curses
+    keypad(stdscr, 1); // allow arrows, F1 - F12
+	
+    noecho(); // don't display user input
     curs_set(0); // hide cursor
+	
     getmaxyx(stdscr, rows, cols);
+	//printw("rows: %d, cols: %d", rows, cols);
 
     char map[rows][cols]; // 0 -- 119  0 -- 23
 	
     do
     {
-        game_loop(c, rows - 1, cols - 1, map);
-
+        c = game_loop(rows - 1, cols - 1, map); // rows-1 cause map[][] 0 ... 119
     }
-    while ((c = getch()) != 27); // 27 = Esc
+    while (c != 27); // 27 = Esc
 	
-    getch();
-
+    refresh();
+	getch();
     endwin();
 	
     return 0;
